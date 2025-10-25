22f6174ace2e099c80bd3da65cb197f7e7a4b710
dineshkumar <dineshkumar22106007@gmail.com>
2025-10-25 08:15:43 +0530

    Initial commit

---
+++ datasetDownloadFromGoogle.py
@@ -0,0 +1,110 @@
+
+
+#Data's Collection or Creation from google
+
+
+from pygoogle_image import image as pi
+
+pi.download("jhon cena",limit=50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##
+##import time
+##for i in range(20):
+##    pi.download("RomanReigns", limit=1)
+##    time.sleep(2)  # Wait 2 seconds between requests
+
+
+##from bing_image_downloader import downloader
+##
+##downloader.download("Roman Reigns", limit=20, output_dir='images', adult_filter_off=True, force_replace=False, timeout=60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##from simple_image_download import simple_image_download as sim
+##
+##response = sim.simple_image_download
+##
+##response().download('harrypotter', 60)
+
+#print(response().urls('batmanjoker', 45))
+
+#response().download('batmanjoker', 5,extensions={'.jpg'})
+#print(response().urls('batmanjoker', 5,extensions={'.jpg'})
+
+
+
